07980a59e99d0541cdce74781fb3646616ca5f04
AhmaDev <ahmadwig@gmail.com>
2022-03-27 13:46:36 +0300

    FIX

--- app.js
+++ app.js
@@ -3,19 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-const history = require("connect-history-api-fallback");
+
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-   history({
-      disableDotRule: true,
-      verbose: true,
-   })
-);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
