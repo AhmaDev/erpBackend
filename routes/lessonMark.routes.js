c5c7fe69c4ed45ee7412cba33d45e6456dd7ec23
Ahmad Faris <ahmadev@Ahmads-MacBook-Pro.local>
2022-05-14 22:30:40 +0300

    fix

--- routes/lessonMark.routes.js
+++ routes/lessonMark.routes.js
@@ -1,13 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const lessonMark = require('../controllers/lessonMark.controllers');
-var auth = require('../middlewares/auth.middlewares');
+const lessonMark = require("../controllers/lessonMark.controllers");
+var auth = require("../middlewares/auth.middlewares");
 
-router.get('/lessonMarks/', lessonMark.findAll);
-router.get('/markTypes/', lessonMark.findAllMarkTypes);
-router.post('/addLessonMark/', auth.roles('REP'), lessonMark.create);
-router.get('/lessonMark/:id', lessonMark.findOne);
-router.put('/lessonMark/:id', auth.roles('REP'), lessonMark.updateOne);
-router.delete('/lessonMark/:id', auth.roles('REP'), lessonMark.deleteOne);
+router.get("/lessonMarks/", lessonMark.findAll);
+router.get("/markTypes/", lessonMark.findAllMarkTypes);
+router.get("/allMarkTypes/", lessonMark.findAllMarkTypesWithHidden);
+router.post("/addLessonMark/", auth.roles("REP"), lessonMark.create);
+router.get("/lessonMark/:id", lessonMark.findOne);
+router.put("/lessonMark/:id", auth.roles("REP"), lessonMark.updateOne);
+router.delete("/lessonMark/:id", auth.roles("REP"), lessonMark.deleteOne);
 
 module.exports = router;
